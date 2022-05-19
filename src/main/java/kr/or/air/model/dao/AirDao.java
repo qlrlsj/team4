afea5e6dc1597f4a06453c024eabe3cbaed5f28e
himer <himer@DESKTOP-NM7K0UQ>
2022-05-19 14:25:45 +0900

    일단 임시

--- src/main/java/kr/or/air/model/dao/AirDao.java
+++ src/main/java/kr/or/air/model/dao/AirDao.java
@@ -1,5 +1,11 @@
 package kr.or.air.model.dao;
 
-public class AirDao {
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
+@Repository
+public class AirDao {
+	@Autowired
+	private SqlSessionTemplate sqlsession;
 }
