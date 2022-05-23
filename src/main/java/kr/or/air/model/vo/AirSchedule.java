071ade37cc51a58aca105ed477e348a0c4f28742
himer <himer@DESKTOP-NM7K0UQ>
2022-05-23 16:35:31 +0900

    v1.5 air 예매 달력까지구현

---
+++ src/main/java/kr/or/air/model/vo/AirSchedule.java
@@ -0,0 +1,18 @@
+package kr.or.air.model.vo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class AirSchedule {
+	private String airLine;			//항공사
+	private String airNumber;		//편명
+	private String airStartTime;	//출발시간
+	private String airEndTime;		//도착시간
+	private String airStart;		//출발지
+	private String airArrive;		//도착지
+	private String airDate;			//날짜
+}
