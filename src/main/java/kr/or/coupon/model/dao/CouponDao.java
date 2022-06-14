878ac790bcdc27133f21f473734ab4d2d0f008eb
himer <himer@DESKTOP-NM7K0UQ>
2022-06-14 21:15:36 +0900

    여러가지수정

--- src/main/java/kr/or/coupon/model/dao/CouponDao.java
+++ src/main/java/kr/or/coupon/model/dao/CouponDao.java
@@ -35,5 +35,22 @@ public class CouponDao {
 		int result = sqlSession.insert("coupon.couponMake",coupon);
 		return result;
 	}
+
+	public ArrayList<Coupon> selectCoupon() {
+		List list = sqlSession.selectList("coupon.selectCoupon");
+		return (ArrayList<Coupon>)list;
+	}
+
+
+	public int couponSend(int couponNo, String id) {
+		// TODO Auto-generated method stub
+		int memberNo = sqlSession.selectOne("coupon.findMemberNo",id);
+		HashMap<Object, Object> map = new HashMap<Object, Object>();
+		map.put("memberNo", memberNo);
+		map.put("couponNo", couponNo);
+		int result = sqlSession.insert("coupon.couponSend",map);
+		
+		return 0;
+	}
 	
 }
